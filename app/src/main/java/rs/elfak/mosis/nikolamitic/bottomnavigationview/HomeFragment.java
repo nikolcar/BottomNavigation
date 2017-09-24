20cb7c6f4ff1fa4edf57157e7a806e0a487f1211
nikolcar <mitic.nikolca94@gmail.com>
2017-09-24 14:51:22 +0200

    Home Mapa, Marker, Search, Dropdown

---
+++ app/src/main/java/rs/elfak/mosis/nikolamitic/bottomnavigationview/HomeFragment.java
@@ -0,0 +1,131 @@
+package rs.elfak.mosis.nikolamitic.bottomnavigationview;
+
+import android.Manifest;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class HomeFragment extends Fragment
+{
+    private GoogleMap googleMap;
+    MapView mMapView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.home_fragment, container, false);
+
+        mMapView = (MapView) view.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.onResume(); // needed to get the map to display immediately
+
+        try
+        {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        mMapView.getMapAsync(new OnMapReadyCallback()
+        {
+            @Override
+            public void onMapReady(GoogleMap mMap)
+            {
+                googleMap = mMap;
+
+                googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+
+                // For showing a move to my location button
+                if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                googleMap.setMyLocationEnabled(false);
+
+                LatLng currentLocation = new LatLng(43.3246,21.9030);
+
+                // For dropping a marker at a point on the Map
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(currentLocation);
+
+                Bitmap markImage = BitmapFactory.decodeResource(getResources(), R.mipmap.me);
+                markerOptions.icon(BitmapDescriptorFactory.fromBitmap(bitmapSizeByScall(markImage,0.1f)));
+                markerOptions.title("You");
+
+                googleMap.addMarker(markerOptions);
+
+                // For zooming automatically to the location of the marker
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(currentLocation).zoom(10).build();
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        });
+
+
+        return view;
+    }
+
+    public Bitmap bitmapSizeByScall( Bitmap bitmapIn, float scall_zero_to_one_f)
+    {
+        Bitmap bitmapOut = Bitmap.createScaledBitmap(bitmapIn,
+                Math.round(bitmapIn.getWidth() * scall_zero_to_one_f),
+                Math.round(bitmapIn.getHeight() * scall_zero_to_one_f), false);
+
+        return bitmapOut;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
+}
--- app/src/main/java/rs/elfak/mosis/nikolamitic/bottomnavigationview/MainActivity.java
+++ app/src/main/java/rs/elfak/mosis/nikolamitic/bottomnavigationview/MainActivity.java
@@ -55,7 +55,7 @@ public class MainActivity extends Activity {
         switch (name)
         {
             case "home":
-                newFragment = new FragmentHome();
+                newFragment = new HomeFragment();
                 break;
             case "friends":
                 newFragment = new FriendsFragment();
